db25a40ea4265ec89978c8743997b15435ddd131
Kevin <kevin@k-ten.de>
2023-01-01 04:15:26 +0100

    Base features

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,66 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Box from './Box.svelte';
+	var timerVariable = setInterval(countUpTimer, 1000);	
+	
+
+	
+	let todos = [
+		{ done: false, text: 'finish Svelte tutorial', timerrun: false, seconds: 0 },
+		{ done: false, text: 'build an app', timerrun: false, seconds: 0 },
+		{ done: false, text: 'world domination', timerrun: false, seconds: 0}
+	];
+	$: remaining = todos.filter(t => !t.done).length;
+
+	function countUpTimer(){
+		for (let i = 0; i < remaining; i++){
+			if(todos[i].timerrun == true){
+				todos[i].seconds = todos[i].seconds +1;
+			}
+
+
+		}
+		
+
+	}
+
+
+	function add() {
+		todos = todos.concat({ done: false, text: '' , timerrun: false, seconds: 0});
+		timers = timers.concat({seconds: 0, minutes: 0, timerrun: false, seconds: 0});
+	}
+
+	function clear() {
+		todos = todos.filter(t => !t.done);
+	}
+
+</script>
+
+<h1>Todos</h1>
+
+{#each todos as todo}
+	<div>
+		<Box>
+		<input	type=checkbox bind:checked={todo.done}>
+		<input placeholder="What needs to be done?" bind:value={todo.text}	disabled={todo.done}	>
+			<br>
+		<input	type=checkbox bind:checked={todo.timerrun}	>
+		<input type=text bind:value={todo.seconds}>
+		
+		<p><strong>Minuten:</strong> {todo.seconds / 60}</p>
+
+
+
+
+		</Box>
+	</div>
+{/each}
+
+<p>{remaining} remaining</p>
+
+<button on:click={add}>
+	Add new
+</button>
+
+<button on:click={clear}>
+	Clear completed
+</button>
